1dab330c856a0c648162595765d3b82dc1b9eed9
Cibele Martins <cibelemartins24@hotmail.com>
2022-06-05 19:38:51 -0300

    :package: feat: add v-parallax

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,45 @@
 <template>
   <v-app dark>
-    
 
     <v-main>
-      <v-container>
+      <v-toolbar
+      color="purple"
+      dark
+      >
+      <v-toolbar-title>CLASSY</v-toolbar-title>
+
+      <v-divider
+        class="mx-4"
+        vertical
+      ></v-divider>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>
+          HOME
+        </v-btn>
+
+        <v-divider vertical></v-divider>
+
+        <v-btn text>
+          ABOUT
+        </v-btn>
+
+        <v-divider vertical></v-divider>
+
+        <v-btn text>
+          CONTACT
+        </v-btn>
+
+        <v-divider vertical></v-divider>
+      </v-toolbar-items>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </v-toolbar>
+    
+      <v-container
+      w="100vh">
         <Nuxt />
       </v-container>
     </v-main>
--- pages/teste.vue
+++ pages/teste.vue
@@ -1,39 +1,12 @@
 <template>
-  <v-toolbar
-    color="purple"
-    dark
-  >
-    <v-toolbar-title>CLASSY</v-toolbar-title>
 
-    <v-divider
-      class="mx-4"
-      vertical
-    ></v-divider>
 
-    <v-spacer></v-spacer>
+ <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
 
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn text>
-        HOME
-      </v-btn>
+ 
+</template>
 
-      <v-divider vertical></v-divider>
 
-      <v-btn text>
-        ABOUT
-      </v-btn>
-
-      <v-divider vertical></v-divider>
-
-      <v-btn text>
-        CONTACT
-      </v-btn>
-
-      <v-divider vertical></v-divider>
-    </v-toolbar-items>
-
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  </v-toolbar>
 </template>
 
 <script>
